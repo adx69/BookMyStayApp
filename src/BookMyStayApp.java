5b4afe5dfa9931bd33446e74aa545bb67b3bbc53
ADX <ar6553@gmail.com>
2026-03-13 14:39:04 +0530

    UC2

--- src/BookMyStayApp.java
+++ src/BookMyStayApp.java
@@ -2,14 +2,90 @@ public class BookMyStayApp {
 
     public static void main(String[] args) {
 
-        String applicationName = "BookMyStay Hotel Booking System";
-        String version = "v1.0";
+        Room singleRoom = new SingleRoom();
+        Room doubleRoom = new DoubleRoom();
+        Room suiteRoom = new SuiteRoom();
 
-        System.out.println("======================================");
-        System.out.println(" Welcome to " + applicationName);
-        System.out.println(" Version: " + version);
-        System.out.println(" Application started successfully.");
-        System.out.println("======================================");
+        int singleRoomAvailability = 5;
+        int doubleRoomAvailability = 3;
+        int suiteRoomAvailability = 2;
 
+        System.out.println("===== Room Availability =====\n");
+
+        singleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + singleRoomAvailability);
+        System.out.println();
+
+        doubleRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + doubleRoomAvailability);
+        System.out.println();
+
+        suiteRoom.displayRoomDetails();
+        System.out.println("Available Rooms: " + suiteRoomAvailability);
+
+        System.out.println("\nApplication terminated.");
+    }
+}
+
+
+abstract class Room {
+
+    private String roomType;
+    private int beds;
+    private double size;
+    private double price;
+
+    public Room(String roomType, int beds, double size, double price) {
+        this.roomType = roomType;
+        this.beds = beds;
+        this.size = size;
+        this.price = price;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public int getBeds() {
+        return beds;
+    }
+
+    public double getSize() {
+        return size;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void displayRoomDetails() {
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Beds: " + beds);
+        System.out.println("Room Size: " + size + " sqm");
+        System.out.println("Price per Night: $" + price);
+    }
+}
+
+
+class SingleRoom extends Room {
+
+    public SingleRoom() {
+        super("Single Room", 1, 20.0, 100.0);
+    }
+}
+
+
+class DoubleRoom extends Room {
+
+    public DoubleRoom() {
+        super("Double Room", 2, 35.0, 180.0);
+    }
+}
+
+
+class SuiteRoom extends Room {
+
+    public SuiteRoom() {
+        super("Suite Room", 3, 60.0, 350.0);
     }
 }
